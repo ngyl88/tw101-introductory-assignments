7ca4988117d91371c49e86f4719776b801d4f01d
ngyl88 <ng_yl88@hotmail.com>
2018-09-20 16:38:32 +0800

    Diamond: split main into methods

--- Diamond/Diamond.java
+++ Diamond/Diamond.java
@@ -6,6 +6,37 @@ import java.util.ArrayDeque;
  */
 public class Diamond {
 
+    private static String getFormattedLine(String charSeq, int spaces) {
+        return new String(new char[spaces]).replaceAll("\0", charSeq);
+    }
+
+    private static ArrayDeque<String> printUpperHalfAndReturnPrintedStack(int n) {
+
+        ArrayDeque<String> printedStack = new ArrayDeque<String>();
+
+        String spacesString = null;
+        String printedString = null;
+        for (int i = 0; i < n; i++) {
+
+            spacesString = getFormattedLine(" ", n - (i + 1));
+            printedString = getFormattedLine("*", (i + 1) * 2 - 1);
+
+            String formattedStringToPrint = spacesString + printedString;
+            System.out.println(formattedStringToPrint);
+
+            if (i != n - 1) {
+                printedStack.push(formattedStringToPrint);
+            }
+        }
+        return printedStack;
+    }
+
+    public static void popAndPrintLowerHalf(ArrayDeque<String> printedStack) {
+        while (!printedStack.isEmpty()) {
+            System.out.println(printedStack.pop());
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Please enter n: ");
         Scanner in = new Scanner(System.in);
@@ -13,29 +44,10 @@ public class Diamond {
         try {
             int n = in.nextInt();
 
-            ArrayDeque<String> stringPrinted = new ArrayDeque<String>();
-            int lineToPrint = n * 2 - 1;
+            ArrayDeque<String> stringPrinted = printUpperHalfAndReturnPrintedStack(n);
 
-            String spacesString = null;
-            String printedString = null;
-            for (int i = 0; i < n; i++) {
-                int spaces = n - (i + 1);
-                spacesString = new String(new char[spaces]).replaceAll("\0", " ");
+            popAndPrintLowerHalf(stringPrinted);
 
-                int currentToPrint = (i + 1) * 2 - 1;
-                printedString = new String(new char[currentToPrint]).replaceAll("\0", "*");
-
-                String formattedStringToPrint = spacesString + printedString;
-                System.out.println(formattedStringToPrint);
-
-                if(i != n - 1) {
-                    stringPrinted.push(formattedStringToPrint);
-                }
-            }
-            
-            for (int i = lineToPrint - n; i > 0; i--) {
-                System.out.println(stringPrinted.pop());
-            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
