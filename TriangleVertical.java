c1cd5f34f7554e02b4c9ff229344efa7fa9aa68f
ngyl88 <ng_yl88@hotmail.com>
2018-09-20 15:20:34 +0800

    Triangle ex2 and ex3

--- TriangleVertical.java
+++ TriangleVertical.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
 
 /**
- * TriangleHorizontal
+ * TriangleVertical
  */
-public class TriangleHorizontal {
+public class TriangleVertical {
+
     public static void main(String[] args) {
         System.out.println("Please enter n: ");
         Scanner in = new Scanner(System.in);
         try {
             int n = in.nextInt();
-            System.out.print(new String(new char[n]).replaceAll("\0", "*"));
+            
+            for (int i = 0; i < n; i++) {
+                System.out.println("*");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             in.close();
         }
-    }    
+    }
 }
